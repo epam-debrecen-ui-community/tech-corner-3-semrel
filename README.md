58a4bad0afee976da9e2abc0abf3f634fd28ec84
attilagyongyosi <attilagyongyosi0@gmail.com>
2021-02-26 00:13:25 +0000

    docs: adding initial README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# EPAM Debrecen UI Community
+## Tech Corner 3 - The semantic-release Toolset
--- release.config.js
+++ release.config.js
@@ -1,6 +1,13 @@
 module.exports = {
     plugins: [
-        '@semantic-release/commit-analyzer',
+        ['@semantic-release/commit-analyzer', {
+            'preset': 'angular',
+            'releaseRules': [
+                { type: 'docs', release: 'patch' },
+                { type: 'refactor', release: 'patch' },
+                { type: 'style', release: 'patch' }
+            ]
+        }],
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
         ['@semantic-release/npm', {
